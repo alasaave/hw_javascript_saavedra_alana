ab08d315130378c60713b3abc5b3859adcee0750
Alana Saavedra <alasaave@gmail.com>
2018-02-04 21:27:22 -0600

    Add function expression using string length

--- js/main.js
+++ js/main.js
@@ -14,3 +14,16 @@ function howdy() {
     console.log('Alana Saavedra says hi!');
 }
 howdy();
+
+var numba = function (string) {
+    if (string.length < 7) {
+        console.log('What a short little word!');
+    } else if (string.length === 7) {
+        console.log('7, what a perfect choice!');
+    } else {
+        console.log("I'm sorry, but that's too many to count.");
+    }
+};
+numba('7');
+numba('seventy');
+numba('university');
